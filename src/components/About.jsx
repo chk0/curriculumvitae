2f21b995959b20ae0243a13b88b6c8756a9d3e23
Sergio Salazar <ssalazarpalacios@gmail.com>
2019-09-09 01:55:08 +0000

    modificando color de h2 y del borde de la foto, probando ditpod.io

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,10 @@ const AboutAvatar = styled.div`
 `;
 
 const AboutImg = styled.img`
-    border-radius: 100%;
+    border-radius: 2px;
     width: 160px;
     height: 160px;
-    border: 2px solid #E91E63;
+    border: 1px solid #17e0e0;
     margin: 0 auto;
     display: block;
     box-shadow: 0 0 10px rgba(0,0,0,0.6);
@@ -29,14 +29,14 @@ const AboutH2 = styled.h2`
     font-weight: 400;
     letter-spacing: 1.2px;
     margin: .5em 0 0 0;
-    color: #C2185B;    
+    color: #17e0e0;
 `;
 
 const AboutProfession = styled.p`
     margin: .2em 0 1em 0;
     letter-spacing: 1.6px;
     font-weight: 300;
-    color: #C2185B;
+    color: #17e0e0;
 `;
 
 const AboutBio = styled.p`
--- src/styled/H2Styled.jsx
+++ src/styled/H2Styled.jsx
@@ -6,7 +6,7 @@ const StyledH2 = styled.h2`
     font-weight: 300;
     letter-spacing: .8px;
     margin: 1em 0 0 0;
-    color: #C2185B;
+    color: #17e0e0;
 `;
 
 const H2Styled = ({ name }) => <StyledH2>{name}</StyledH2>;
